9144c20878bae663c901cc707338d4d3c2075dc5
Soter Ramirez <soterrc@gmail.com>
2022-05-20 13:05:50 -0500

    Header de autorización

--- public/js/dog.js
+++ public/js/dog.js
@@ -1,7 +1,7 @@
 //The Dog API
-const API_URL_RANDOM = "https://api.thedogapi.com/v1/images/search?limit=4&api_key=ae4e08e8-4f4c-4f0b-8159-98b33b9d46f2";
-const API_URL_FAVORITE = "https://api.thedogapi.com/v1/favourites?api_key=ae4e08e8-4f4c-4f0b-8159-98b33b9d46f2";
-const API_URL_FAVORITE_DELETE = (id) => `https://api.thedogapi.com/v1/favourites/${id}?api_key=ae4e08e8-4f4c-4f0b-8159-98b33b9d46f2`;
+const API_URL_RANDOM = "https://api.thedogapi.com/v1/images/search?limit=4";
+const API_URL_FAVORITE = "https://api.thedogapi.com/v1/favourites?api_key=";
+const API_URL_FAVORITE_DELETE = (id) => `https://api.thedogapi.com/v1/favourites/${id}?api_key=`;
 const spanError = document.getElementById("Error");
 //Load Random Dog Image
 async function loadRandomDog() {
@@ -37,7 +37,12 @@ async function loadRandomDog() {
 }
 //Load Favorite Dog Image
 async function loadFavoriteDog() {
-    const response = await fetch(API_URL_FAVORITE);
+    const response = await fetch(API_URL_FAVORITE, {
+        method: "GET",
+        headers: {
+            'X-API-KEY': 'ae4e08e8-4f4c-4f0b-8159-98b33b9d46f2',
+        },
+    });
     const data = await response.json();
     console.log("Load Favorite Dog");
     console.log(data);
@@ -79,6 +84,7 @@ async function saveFavoriteDog(id) {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
+            'X-API-KEY': 'ae4e08e8-4f4c-4f0b-8159-98b33b9d46f2',
         },
         body: JSON.stringify({
             image_id: id,
@@ -98,6 +104,9 @@ async function saveFavoriteDog(id) {
 async function deleteFavoriteDog(id) {
     const response = await fetch(API_URL_FAVORITE_DELETE(id), {
         method: "DELETE",
+        headers: {
+            'X-API-KEY': 'ae4e08e8-4f4c-4f0b-8159-98b33b9d46f2',
+        },
     });
     const data = await response.json();
     if (response.status !== 200) {
